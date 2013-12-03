ca83e666ac96c67607d3a2932041a4f3671927e0
Vincent Cabansag <vcabansag@gmail.com>
2013-12-03 11:15:07 -0600

    Scaffolded a basic User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203171302) do
+ActiveRecord::Schema.define(version: 20131203171416) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
